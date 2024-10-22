fc5b76be25bd6fae98512d850b072d22f6b638a4
Okpah Evans Chukwunnaemeka <okpahevans@gmail.com>
2024-10-22 06:30:53 -0700

    Update 2-redis_op_async.js

--- 0x03-queuing_system_in_js/2-redis_op_async.js
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -4,27 +4,27 @@ import { promisify } from 'util';
 const client = createClient();
 
 client.on('connect', function() {
-	  console.log('Redis client connected to the server');
+  console.log('Redis client connected to the server');
 });
 
 client.on('error', function (err) {
-	  console.log(`Redis client not connected to the server: ${err}`);
+  console.log(`Redis client not connected to the server: ${err}`);
 });
 
 function setNewSchool(schoolName, value) {
-	  client.set(schoolName, value, print);
+  client.set(schoolName, value, print);
 };
 
 const get = promisify(client.get).bind(client);
 
 async function displaySchoolValue(schoolName) {
-	  const result = await get(schoolName).catch((error) => {
-		      if (error) {
-			            console.log(error);
-			            throw error;
-			          }
-		    });
-	  console.log(result);
+  const result = await get(schoolName).catch((error) => {
+    if (error) {
+      console.log(error);
+      throw error;
+    }
+  });
+  console.log(result);
 }
 
 displaySchoolValue('Holberton');

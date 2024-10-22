8ab65e168dbcbd2cc8135a5cbd3b6e5d2487e692
Okpah Evans Chukwunnaemeka <okpahevans@gmail.com>
2024-10-22 06:35:47 -0700

    Update 6-job_creator.js

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -3,18 +3,18 @@ import { createQueue } from 'kue';
 const queue = createQueue();
 
 const notification = {
-	  'phoneNumber': '4153518780',
-	  'message': 'This is the code to verify your account'
+  'phoneNumber': '4153518780',
+  'message': 'This is the code to verify your account'
 }
 
 const job = queue.create('push_notification_code', notification).save(function (error) {
-	  if (!error) {
-		      console.log(`Notification job created: ${job.id}`);
-		    }
+  if (!error) {
+    console.log(`Notification job created: ${job.id}`);
+  }
 });
 
 job.on('complete', function() {
-	    console.log('Notification job completed');
+    console.log('Notification job completed');
 }).on('failed', function() {
-	    console.log('Notification job failed');
+    console.log('Notification job failed');
 });

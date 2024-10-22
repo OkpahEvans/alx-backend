fa48de938013cc5e3dd87a7b95cf159c961f7b17
Okpah Evans Chukwunnaemeka <okpahevans@gmail.com>
2024-10-22 06:25:37 -0700

    Update 1-redis_op.js

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -3,25 +3,25 @@ import { createClient, print } from 'redis';
 const client = createClient();
 
 client.on('connect', function() {
-	  console.log('Redis client connected to the server');
+  console.log('Redis client connected to the server');
 });
 
 client.on('error', function (err) {
-	  console.log(`Redis client not connected to the server: ${err}`);
+  console.log(`Redis client not connected to the server: ${err}`);
 });
 
 function setNewSchool(schoolName, value) {
-	  client.set(schoolName, value, print);
+  client.set(schoolName, value, print);
 };
 
 function displaySchoolValue(schoolName) {
-	  client.get(schoolName, function(error, result) {
-		      if (error) {
-			            console.log(error);
-			            throw error;
-			          }
-		      console.log(result);
-		    });
+  client.get(schoolName, function(error, result) {
+    if (error) {
+      console.log(error);
+      throw error;
+    }
+    console.log(result);
+  });
 }
 
 displaySchoolValue('Holberton');

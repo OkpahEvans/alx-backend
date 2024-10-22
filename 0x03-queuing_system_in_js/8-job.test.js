d41c40bdce030f6ddb48fb8cb6fd4c26d44f38a3
Okpah Evans Chukwunnaemeka <okpahevans@gmail.com>
2024-10-22 06:42:36 -0700

    Update 8-job.test.js

--- 0x03-queuing_system_in_js/8-job.test.js
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -7,42 +7,42 @@ import createPushNotificationsJobs from './8-job.js';
 const queue = createQueue();
 
 describe('Test createPushNotificatinsJobs function', function() {
-	  before(function () {
-		      queue.testMode.enter();
-		    });
+  before(function () {
+    queue.testMode.enter();
+  });
 
-	  afterEach(function () {
-		      queue.testMode.clear();
-		    });
+  afterEach(function () {
+    queue.testMode.clear();
+  });
 
-	  after(function () {
-		      queue.testMode.exit();
-		    });
+  after(function () {
+    queue.testMode.exit();
+  });
 
-	  it('display an error message if jobs is not an array', function() {
-		      expect(() => createPushNotificationsJobs('job', queue)).to.throw(Error, 'Jobs is not an array');
-		    });
+  it('display an error message if jobs is not an array', function() {
+    expect(() => createPushNotificationsJobs('job', queue)).to.throw(Error, 'Jobs is not an array');
+  });
 
-	  it('Test whether jobs are created', function() {
-		      const jobs = [
-			            {
-					            phoneNumber: '4153518780',
-					            message: 'This is the code 1234 to verify your account'
-					          },
-			            {
-					            phoneNumber: '4153518781',
-					            message: 'This is the code 4562 to verify your account'
-					          },
-			          ];
+  it('Test whether jobs are created', function() {
+    const jobs = [
+      {
+        phoneNumber: '4153518780',
+        message: 'This is the code 1234 to verify your account'
+      },
+      {
+        phoneNumber: '4153518781',
+        message: 'This is the code 4562 to verify your account'
+      },
+    ];
 
-		      createPushNotificationsJobs(jobs, queue);
+    createPushNotificationsJobs(jobs, queue);
 
-		      expect(queue.testMode.jobs.length).to.equal(2);
+    expect(queue.testMode.jobs.length).to.equal(2);
 
-		      expect(queue.testMode.jobs[0].type).to.equal('push_notification_code_3');
-		      expect(queue.testMode.jobs[0].data).to.eql(jobs[0]);
+    expect(queue.testMode.jobs[0].type).to.equal('push_notification_code_3');
+    expect(queue.testMode.jobs[0].data).to.eql(jobs[0]);
 
-		      expect(queue.testMode.jobs[1].type).to.equal('push_notification_code_3');
-		      expect(queue.testMode.jobs[1].data).to.eql(jobs[1]);
-		    });
+    expect(queue.testMode.jobs[1].type).to.equal('push_notification_code_3');
+    expect(queue.testMode.jobs[1].data).to.eql(jobs[1]);
+  });
 });
